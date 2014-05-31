220dfe6be9b8104a02f480a2e0e70a77698e986f
k3b <klaus3b-github@yahoo.de>
2014-05-31 19:08:04 +0200

    first attempts to use roboelectric

---
+++ libIcs4Android/src/androidTest/java/de/k3b/android/calendar/ExportTest.java
@@ -0,0 +1,35 @@
+package de.k3b.android.calendar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import net.fortuna.ical4j.model.Calendar;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by k3b on 31.05.2014.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ExportTest {
+    // @Test
+    public void shouldFormat() {
+        Context c = new Activity();
+        final Uri data = null;
+        ACalendar2IcsEngine engine = new ACalendar2IcsEngine(c, true);
+        Calendar vcalendar = engine.export(data);
+
+        System.out.println(vcalendar.toString());
+
+        Assert.assertEquals(false, true);
+    }
+
+    @Test
+    public void testDatabaseMock() {
+    }
+}
